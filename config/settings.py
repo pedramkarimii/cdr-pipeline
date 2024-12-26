1a43f53109dc246b3f36b5d3af1766823ab4e44a
pedramkarimii <pedram@9060.com>
2024-12-26 23:54:22 +0330

    :zap:  Add Django settings for RabbitMQ, Elasticsearch, JWT, and caching

---
+++ config/settings.py
@@ -0,0 +1,208 @@
+import os
+from pathlib import Path
+from decouple import config  # noqa
+from datetime import timedelta
+
+USE_TZ = True
+USE_I18N = True
+LANGUAGE_CODE = "en-us"
+ROOT_URLCONF = "config.urls"
+WSGI_APPLICATION = "config.wsgi.application"
+TIME_ZONE = config("TIME_ZONE", default="UTC")
+DEBUG = config("DEBUG", cast=bool, default=True)
+BASE_DIR = Path(__file__).resolve().parent.parent
+APP_DIR = BASE_DIR / "apps"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SECRET_KEY = config("SECRET_KEY", default="secret-key-!!!")
+ALLOWED_HOSTS = (
+    ["*"]
+    if DEBUG
+    else config(
+        "ALLOWED_HOSTS", cast=lambda host: [h.strip() for h in host.split(",") if h]
+    )
+)
+
+AUTH_PASSWORD_VALIDATORS = [
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+if not DEBUG:
+    MIDDLEWARE.append('apps.core.middlewares.LoginRequiredMiddleware')
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / ""],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# THROTTLE_CONFIG
+THROTTLE_CONFIG = {
+    'default': {
+        'rate': '5/m',  # 5 requests per 5 minutes
+    },
+    'admin': {
+        'rate': '20/m',  # 20 requests per 5 minutes
+    },
+}
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework.simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework.simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework.simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_THROTTLING_CLASSES': [
+        'apps.account.throttling.CustomThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'default': '5/m',
+        'admin': '20/m',
+    },
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'AVA',
+    'DESCRIPTION': 'CDR Pipeline is a scalable and reliable system for ingesting, processing, and searching Call Detail'
+                   ' Records (CDRs).',
+    'VERSION': '1.0.0',
+}
+# Applications
+APPLICATIONS = ["cdr", ]
+
+# Serving
+STATIC_URL = "storage/static/"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "storage/media"
+
+# Logging
+LOG_FILE_PATH = config("LOG_FILE_PATH")
+
+# RabbitMQ settings for Celery
+CELERY_BROKER_URL = config("CELERY_BROKER_URL")
+CELERY_ACCEPT_CONTENT = config("CELERY_ACCEPT_CONTENT")
+CELERY_TASK_SERIALIZER = config("CELERY_TASK_SERIALIZER")
+
+# Elasticsearch Settings
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': config('ELASTICSEARCH_HOST', default='http://localhost:9200')
+    },
+}
+ELASTICSEARCH_DSL['default'].update({
+    'TIMEOUT': 60,
+    'RETRY_ON_TIMEOUT': True,
+    'MAX_RETRIES': 3,
+})
+ELASTICSEARCH_INDEX_SETTINGS = {
+    'number_of_shards': 4,
+    'number_of_replicas': 0,
+}
+
+# Mode Handling:
+if DEBUG:
+
+    INSTALLED_APPS = [
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        # Third-party
+        "rest_framework",
+        "rest_framework.authtoken",
+        "drf_spectacular",
+        # Application
+        *list(map(lambda app: f"apps.{app}", APPLICATIONS)),
+    ]
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST"),
+            "PORT": config("DB_PORT"),
+        }
+    }
+
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+            "LOCATION": BASE_DIR / "utility/cache",
+        }
+    }
+
+else:
+    CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS")
+    STATIC_ROOT = BASE_DIR / "storage/static/"
+    INSTALLED_APPS = [
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        # Third-party
+        "rest_framework",
+        # Application
+        *list(map(lambda app: f"apps.{app}", APPLICATIONS)),
+    ]
+    REDIS_URL = f"redis://{config('REDIS_HOST')}:{config('REDIS_PORT')}"
+
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": REDIS_URL,
+        }
+    }
+    SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
