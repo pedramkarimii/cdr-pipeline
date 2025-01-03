01bd0d6a57e7dd655a40dacf0f8411869c375cee
pedramkarimii <pedram@9060.com>
2025-01-03 14:28:32 +0330

    :wheelchair: Update RabbitMQ consumer to inherit from RabbitMQMain and improve error handling

--- apps/cdr/tasks/tasks_consumer.py
+++ apps/cdr/tasks/tasks_consumer.py
@@ -1,52 +1,25 @@
-from apps.core import os_setting_elastic
-import pika
+from apps.core import os_setting_elastic  # noqa
 import json
 from django.utils.dateparse import parse_datetime
 from apps.cdr.models import Cdr
 from django.utils import timezone
-from urllib.parse import urlparse
+from apps.cdr.tasks.tasks_main import RabbitMQMain
 
 
-class RabbitMQConsumer:
-    def __init__(self, queue_prefix, shard_count, url='amqps://pdtwxisb:JSRKhYIwoER7i99A1BjqP1CRQstZvTr7@possum.lmq.cloudamqp.com/pdtwxisb', host='localhost', port=5672, username='guest',
-                 password='guest', virtual_host='/', max_retries=5, retry_delay=2):
-
-        """
-        Initialize the RabbitMQConsumer with connection parameters and shard configuration.
-        """
-        self.queue_prefix = queue_prefix
-        self.shard_count = shard_count
-        self.max_retries = max_retries
-        self.retry_delay = retry_delay
-        self.url = url
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
-        self.virtual_host = virtual_host
-        self.connection = None
-        self.channel = None
+class RabbitMQConsumer(RabbitMQMain):
+    """
+    This class is used for consuming messages from RabbitMQ queues, processing them into Call Detail Records (CDRs),
+    and saving them to a database.
+    """
 
     def connect(self):
         """Establish connection to RabbitMQ."""
-        credentials = pika.PlainCredentials(self.username, self.password)
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=self.host,
-                port=self.port,
-                credentials=credentials,
-                virtual_host=self.virtual_host
-            )
-        )
-        self.channel = self.connection.channel()
-
+        super().connect()
         for shard_id in range(self.shard_count):
-            queue_name = f"{self.queue_prefix}_{shard_id}"
-            self.channel.queue_declare(queue=queue_name, durable=True)
-            self.channel.basic_consume(queue=queue_name, on_message_callback=self.process_message, auto_ack=False)
-        print(f"Connected to RabbitMQ at {self.host}:{self.port} and consuming messages...")
+            self.channel.basic_consume(queue=f"{self.queue_prefix}_{shard_id}",
+                                       on_message_callback=self.process_message, auto_ack=False)
 
-    def process_message(self, ch, method, properties, body):
+    def process_message(self, ch, method, properties, body):  # noqa
         """
         This method will be called for each message consumed from the RabbitMQ queue.
         """
@@ -62,7 +35,7 @@ class RabbitMQConsumer:
             print(f"Error processing message: {e}. Skipping message: {body}")
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
-    def _parse_message(self, message):
+    def _parse_message(self, message):  # noqa
         """
         This method parses the message and converts it into the CDR format.
         """
@@ -86,35 +59,20 @@ class RabbitMQConsumer:
         }
         return cdr_data
 
-    def _save_cdr(self, cdr_data):
+    def _save_cdr(self, cdr_data):  # noqa
         """
         Save the CDR data to the database.
         """
-        Cdr.objects.create(
-            src_number=cdr_data['src_number'],
-            dest_number=cdr_data['dest_number'],
-            call_duration=cdr_data['call_duration'],
-            call_successful=cdr_data['call_successful'],
-            start_time=cdr_data['start_time'],
-            end_time=cdr_data['end_time'],
-            timestamp=cdr_data['timestamp']
-        )
+        Cdr.objects.create(**cdr_data)
 
     def start_consuming(self):
         """Start consuming messages."""
-        self.channel.start_consuming()
-
-    def close_connection(self):
-        """Close the RabbitMQ connection."""
-        if self.connection:
-            self.connection.close()
-
-
-if __name__ == "__main__":
-    consumer = RabbitMQConsumer(queue_prefix='cdr_queue', shard_count=4)
-    consumer.connect()
-
-    try:
-        consumer.start_consuming()
-    except KeyboardInterrupt:
-        consumer.close_connection()
+        try:
+            self.channel.start_consuming()
+            print("Consumer started consuming messages...")
+        except KeyboardInterrupt:
+            print("Consumer interrupted by user.")
+        except Exception as e:
+            print(f"Error while consuming: {e}")
+        finally:
+            self.close_connection()
